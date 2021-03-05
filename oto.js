9c602091edc06b89f9d933f4cde03b1adb702413
serhat oguten <sogutenbas@gmail.com>
2021-03-05 21:31:04 +0100

    first commit

---
+++ oto.js
@@ -0,0 +1,26 @@
+let brand = bmw
+let model = 2013
+let color = red
+let customer =1
+if(customer>4){
+    console.log("Uzun sureli musterimiz oldugunuz icin butun marka ve model araciniza bakim yapiyoruz..")
+}
+else if((color==red || color==black)&&(brand==bmw || brand==audi || brand==wv)){
+
+if (brand==bmw && color==red && 2009<model && model<2020){
+    console.log("arabaniz isleme alinmistir..")
+}
+else if((brand==audi) && (2004<model && model<20111 || 2013<model && model<2021)){
+    console.log("arabaniz isleme alinmistir..")
+}
+else if(brand==vw && color==black && 2000<model && model<2019){
+    console.log("arabaniz isleme alinmistir..")
+}
+else if(brand==bmw && color==black && model==2020){
+    console.log("arabaniz isleme alinmistir..")
+}
+else{
+    console.log("Uzgunuz bu araca bakim yapamiyoruz..")
+}
+}
+
